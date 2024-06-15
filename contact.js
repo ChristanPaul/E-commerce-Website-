a5caa757cfdadcb5321a7c37ddafd11cdf1b4810
ChristanPaul <140090355+ChristanPaul@users.noreply.github.com>
2024-06-15 15:14:58 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function shownavbar()
+{
+    sidenav.style.left="0"
+}
+
+function closenavbar()
+{
+    sidenav.style.left="-60%"
+}
+
+
+function showMessage() 
+{
+    alert("Thank you for your message!");
+}
